c4b120e78c1e42f32e51d3fc5b00f545d875aba8
Jayram Manale <jaymanale29@gmail.com>
2022-05-31 18:53:17 +0530

    Profile Image Path update

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import mainImg from './../resources/animate/main.svg';
-import ProfileImg from './../resources/png/profile.jpg';
+import ProfileImg from './../resources/png/profile2.png';
 import getSocialLinkData from './../services/mainService';
 
 const Main = () => {
