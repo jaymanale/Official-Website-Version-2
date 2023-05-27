91b9caf1b0b91348199b834403c56146493e143a
jaymanale <jaymanale29@gmail.com>
2023-05-27 13:17:49 +0530

    Removed online contribution section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/navbar";
 import Main from "./components/main";
 import Experience from "./components/experience";
 import Skills from "./components/skills";
-import OnlineContribution from "./components/onlineContribution";
 import TechnicalProjects from "./components/technicalProjects";
 import Certificates from "./components/certificates";
 
@@ -19,17 +18,6 @@ function App() {
       <Skills />
       <TechnicalProjects />
       <Certificates />
-      <OnlineContribution />
-
-      <div className="text-center colorGradiant text-white">
-        Made with <i className="fas fa-heart text-danger"></i> by
-        <a
-          href="https://github.com/jaymanale"
-          className="text-secondary text-decoration-none text-light"
-        >
-          &nbsp; Jayram Manale
-        </a>
-      </div>
     </React.Fragment>
   );
 }
